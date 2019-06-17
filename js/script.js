8b797821d8f4ccedb3dd5c499cadf165af6066b3
zekeg <zechariah.guadiana@gmail.com>
2019-06-16 22:15:56 -0500

    updated notation

--- js/script.js
+++ js/script.js
@@ -1,54 +1,19 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+
+notes on 6.16.2019 :
+This program contains an array of "quote objects", containing quotations and their metadata
+The program currently takes a quotation at random from the array and displays it along with several pieces of metadata,
+but only if the object has that particular named attribute of metadata,
+
+The program also changes the background color into a randomly gererated rgb value;
+  a function randomRGB() was created to return a random number in the 0-255 range
+
+The program will switch to a new quote at an interval of milliseconds dictated by setInterval()
+
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
@@ -106,6 +71,7 @@ function printQuote(){
     return val
   }
 
+  //we can change the background by creating a string with our randomRGB rule
   document.body.style.backgroundColor =  "rgb(" + randomRGB() + "," + randomRGB() + "," + randomRGB() + ")"
 
   //create the quote p tag inside the htmlString variable
@@ -114,7 +80,7 @@ function printQuote(){
   if(quoteObject.source){
       //first place the quote source in a new p tag
       htmlString+='<p class="source">' + quoteObject.source
-      //check for a citation, then check for a year, if they exist, add them
+      //a series of "if" checks; we check for a citation, then check for a year, if they exist, add them to the htmlString
       if(quoteObject.citation){
         htmlString+='<span class="citation">' + quoteObject.citation + '</span>'
       }
@@ -123,18 +89,18 @@ function printQuote(){
       }
 
       if(quoteObject.tags){
-        var linkStr = '<ul>'
+        var linkStr = '<ul>';
         for(var x = 0; x<quoteObject.tags.length; x++){
-          linkStr+='<li class="tag">' + quoteObject.tags[x] + '</li>'
+          linkStr+='<li class="tag">' + quoteObject.tags[x] + '</li>';
         }
-        htmlString+=linkStr + "</ul>"
+        htmlString+=linkStr + "</ul>";
       }
       //close the paragraph tag
-      htmlString+="</p>"
+      htmlString+="</p>";
   }
   //place the assembled string into the quote-box node
-  quoteBox.innerHTML = htmlString
+  quoteBox.innerHTML = htmlString;
 }
 
 //switch the quote every 25 seconds
-setInterval(printQuote, 25000)
+setInterval(printQuote, 25000);
